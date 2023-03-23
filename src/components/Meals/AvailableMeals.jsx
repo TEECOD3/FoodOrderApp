ec662398b033d5790a9a6aa3934d1870e690c1dc
temitope david <temi236topemicheal@gmail.com>
2023-03-23 05:56:14 +0100

    FEAT:🍿FETCHING MEALS WITH FIREBZE API CUSTOM HOOK

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,41 +1,48 @@
 import React from "react";
+import useHttp from "../../Hooks/use-Http";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import { useEffect, useState } from "react";
 
 function AvailableMeals() {
-  return (
-    <section className={classes.meals}>
-      <Card>
+  const { Error, isLoading, sendrequest } = useHttp();
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchedData = (data) => {
+      const loadedmeals = [];
+
+      for (const key in data) {
+        loadedmeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMeals(loadedmeals);
+    };
+
+    sendrequest(
+      {
+        url: "https://foodorderapp-d2403-default-rtdb.firebaseio.com/meals.json",
+      },
+      fetchedData
+    );
+  }, []);
+
+  if (Error) {
+    return <p className={classes.error}>{Error}!!!</p>;
+  }
+
+  const mealslist = (
+    <Card>
+      {isLoading ? (
+        <p>loading...</p>
+      ) : (
         <ul>
-          {DUMMY_MEALS.map((meal) => (
+          {meals.map((meal) => (
             <MealItem
               key={meal.id}
               id={meal.id}
@@ -45,9 +52,11 @@ function AvailableMeals() {
             />
           ))}
         </ul>
-      </Card>
-    </section>
+      )}
+    </Card>
   );
+
+  return <section className={classes.meals}>{mealslist}</section>;
 }
 
 export default AvailableMeals;
